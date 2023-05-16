fd0cd300be27df6296f736ff3374f72db0d26ae8
Evgeny <bril424@mail.ru>
2023-05-16 15:48:42 +0200

    Modal updated

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import Logo from "./media/Logo.png";
 import style from "./index.module.css";
 
-export default function Modal({ open, onClose, children }) {
-  if (!open) return null;
+export default function Modal({ active, setActive, children }) {
   return (
-    <div onClick={onClose} className={style.overlay}>
+    <div
+      className={[style.modal, active ? style.active : style.modal].join(" ")}
+      onClick={() => setActive(false)}
+    >
+      <div
+        className={[style.content, active ? style.active : style.content].join(
+          " "
+        )}
+        onClick={(event) => event.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
+// open, onClose, children
+{
+  /* <div onClick={onClose} className={style.overlay}>
       <div
         onClick={(event) => {
           event.stopPropagation();
@@ -18,6 +35,5 @@ export default function Modal({ open, onClose, children }) {
         </p>
         <img src={Logo} alt="Logo" />
       </div>
-    </div>
-  );
+    </div> */
 }
--- src/components/MyProjects/index.jsx
+++ src/components/MyProjects/index.jsx
@@ -3,20 +3,19 @@ import Modal from "../Modal";
 import style from "./index.module.css";
 
 export default function MyProjects() {
-  const [openModal, setOpenModal] = useState(false);
+  const [modalActive, setModalActive] = useState(false);
 
   return (
     <section className={style.section} id="project">
       <div className={["wrapper", style.my_projects].join(" ")}>
         <h1>Here are my future projects</h1>
-        <button onClick={() => setOpenModal(true)}>open</button>
-        <Modal open={openModal} onClose={() => setOpenModal(false)}>
-          <button>open</button>
+        <button onClick={() => setModalActive(true)}>open</button>
+        <Modal active={modalActive} setActive={setModalActive}>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-            temporibus consectetur tenetur unde nisi molestiae vel illum! Fugiat
-            rem incidunt quasi voluptatum? Aut, dolor ab? Ratione error
-            perferendis atque cumque!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+            adipisci quae veniam quos aspernatur tempore aliquam? Corporis,
+            ipsa. Ducimus officia fugiat quidem quo ratione saepe perferendis,
+            facere omnis explicabo cum.
           </p>
         </Modal>
       </div>

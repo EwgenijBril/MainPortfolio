1db11bdfd2844b30ede679bfacda24e6c72e1c34
Evgeny <bril424@mail.ru>
2023-05-22 10:05:39 +0200

    Madal updated

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -6,17 +6,13 @@ export default function Project({ id, title, image, discription, link }) {
     <div className={style.project}>
       <h2>{title}</h2>
       <div className={style.info_block}>
-        <div>
-          <img src={image} alt={title} />
-        </div>
-        <div className={style.discription}>
-          <p>{discription}</p>
-          <button>
-            <a href={link} target="blank">
-              {link}
-            </a>
-          </button>
-        </div>
+        <img src={image} alt={title} />
+        <p>{discription}</p>
+        <button className={style.btn}>
+          <a href={link} target="blank">
+            Link
+          </a>
+        </button>
       </div>
     </div>
   );

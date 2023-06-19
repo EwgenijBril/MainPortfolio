b59922b1f28dde9bdb68e35f4ffb026466be9d4f
Evgeny <bril424@mail.ru>
2023-06-19 15:32:24 +0200

    form  NavMenu added

--- src/components/NavMenu/index.jsx
+++ src/components/NavMenu/index.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../Modal";
 import logo from "./media/Logo.png";
 import style from "./index.module.css";
 
 export default function NavMenu() {
+  const [modalActive, setModalActive] = useState(false);
   return (
     <header className={style.header}>
       <nav className={["wrapper", style.nav_menu].join(" ")}>
@@ -19,10 +21,26 @@ export default function NavMenu() {
           <a href="#project">
             <li>Project</li>
           </a>
-          <a href="#">
+          <a onClick={() => setModalActive(true)}>
             <li>Contact</li>
           </a>
         </ul>
+        <div className={style.message}>
+          <Modal active={modalActive} setActive={setModalActive}>
+            <form className={style.info}>
+              <input type="text" name="Your name" placeholder="Your name" />
+              <input type="email" name="email" placeholder="Email" />
+              <textarea
+                name="story"
+                cols="30"
+                rows="10"
+                maxLength="1000"
+                placeholder="Write me"
+              ></textarea>
+              <button type="submit">send</button>
+            </form>
+          </Modal>
+        </div>
       </nav>
     </header>
   );

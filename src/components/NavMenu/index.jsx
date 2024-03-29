da1af58997c609b7334b76241968dacfec10cb91
Evgeny <bril424@mail.ru>
2024-03-29 22:14:38 +0100

    added resposive desing in the menu and ant-design

--- src/components/NavMenu/index.jsx
+++ src/components/NavMenu/index.jsx
@@ -1,28 +1,42 @@
 import logo from "./media/Logo.png";
 import style from "./index.module.css";
+import { MenuOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
 export default function NavMenu() {
+	const [menuActive, setMenuActive] = useState(false);
+
+	const toggleMenu = () => {
+		setMenuActive(!menuActive);
+	};
+
+	const closeMenu = () => {
+		setMenuActive(false);
+	};
 	return (
 		<header className={style.header}>
-			<nav className={["wrapper", style.nav_menu].join(" ")}>
-				<div>
+			<div className={["wrapper", style.menu].join(" ")}>
+				<div className={style.logo_menu}>
 					<img src={logo} alt="logo" />
+					<MenuOutlined className={style.menu_button} onClick={toggleMenu} />
 				</div>
-				<ul className={style.block_menu}>
-					<a href="#about">
-						<li>About</li>
-					</a>
-					<a href="#skills">
-						<li>Skill</li>
-					</a>
-					<a href="#project">
-						<li>Project</li>
-					</a>
-					<a href="tel:+49 176 64084993">
-						<li>Contact</li>
-					</a>
-				</ul>
-			</nav>
+				<nav className={`${style.nav_menu} ${menuActive ? style.active : ""}`}>
+					<ul className={style.block_menu}>
+						<a href="#about" onClick={closeMenu}>
+							<li>About</li>
+						</a>
+						<a href="#skills" onClick={closeMenu}>
+							<li>Skill</li>
+						</a>
+						<a href="#project" onClick={closeMenu}>
+							<li>Project</li>
+						</a>
+						<a href="tel:+49 176 64084993" onClick={closeMenu}>
+							<li>Contact</li>
+						</a>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 }

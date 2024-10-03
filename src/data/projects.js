0dc989c91c3d3834a1a0913f7886bffc8ccdda72
Evgeny <bril424@mail.ru>
2024-10-03 20:08:49 +0200

    added new project Consultant

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,11 @@
 export const projects = [
 	{
 		id: 1,
-		title: "Garden Shop",
-		text: "The website is undergoing maintenance",
-		image: "gardenShop.png",
+		title: "Consultant Wohnwert",
+		image: "berater.webp",
 		discription:
-			"This project is a garden online store, where goods were filtered by price, by promotions and sorted. You can also view a specific category or product, all products or products with promotions.The following libraries have been used, such as react-redux, react-thunk, react-router-dom,  react-router-hash-link.",
-		link: "https://gardenshop-r964.onrender.com",
+			"This is a real landing page where I fixed bugs. Most importantly, I made the layout responsive for mobile devices using Tailwind CSS and the Svelte library",
+		link: "https://berater.wohnwert-deutschland.de/daniel.friesen",
 	},
 	{
 		id: 2,
@@ -16,4 +15,14 @@ export const projects = [
 			"I've created a one-page website to showcase my proficiency in using Tailwind CSS, a library I currently utilized in my projects.",
 		link: "https://tail-wind-ui.vercel.app/",
 	},
+	{
+		id: 3,
+		title: "Garden Shop",
+		text: "The website is undergoing maintenance",
+		image: "gardenShop.png",
+		discription:
+			"This project is a garden online store, where goods were filtered by price, by promotions and sorted. You can also view a specific category or product, all products or products with promotions.The following libraries have been used, such as react-redux, react-thunk, react-router-dom,  react-router-hash-link.",
+		link: "https://gardenshop-r964.onrender.com",
+	},
+	
 ];
